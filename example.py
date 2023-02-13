7a45a261c38505d193f557c04303c02576e7b10e
PimDoos <17709721+PimDoos@users.noreply.github.com>
2023-02-13 22:59:31 +0100

    Add device objects to example script

--- example.py
+++ example.py
@@ -2,23 +2,26 @@ import asyncio
 from sessypy.api import SessyApi
 from sessypy.const import SessyApiCommand
 from config import * # Hide your secrets here
+from sessypy.devices import SessyP1Meter, SessyBattery
 
 async def run():
     print("P1 Status")
     p1_api = SessyApi(SESSY_P1_HOST, SESSY_P1_USERNAME, SESSY_P1_PASSWORD)
-    result = await p1_api.get(SessyApiCommand.P1_STATUS)
+    p1 = SessyP1Meter(p1_api)
+    result = await p1.get_p1_status()
     print(result)
     await p1_api.close()
     print("")
 
     print("Power Status")
     battery_api = SessyApi(SESSY_BATTERY_HOST, SESSY_BATTERY_USERNAME, SESSY_BATTERY_PASSWORD)
-    result = await battery_api.get(SessyApiCommand.POWER_STATUS)
+    battery = SessyBattery(battery_api)
+    result = await battery.get_power_status()
     print(result)
     print("")
 
     print("Power Strategy")
-    result = await battery_api.get(SessyApiCommand.POWER_STRATEGY)
+    result = await battery.get_power_strategy()
     print(result)
     print("")
     
