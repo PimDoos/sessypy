2d5928c88b92d66e34cdf2f3f7ac7f3efda7aecc
PimDoos <17709721+PimDoos@users.noreply.github.com>
2023-02-14 19:30:15 +0100

    Add SessyDevice class

--- example.py
+++ example.py
@@ -6,16 +6,14 @@ from sessypy.devices import SessyP1Meter, SessyBattery
 
 async def run():
     print("P1 Status")
-    p1_api = SessyApi(SESSY_P1_HOST, SESSY_P1_USERNAME, SESSY_P1_PASSWORD)
-    p1 = SessyP1Meter(p1_api)
+    p1 = SessyP1Meter(SESSY_P1_HOST, SESSY_P1_USERNAME, SESSY_P1_PASSWORD)
     result = await p1.get_p1_status()
     print(result)
-    await p1_api.close()
+    await p1.close()
     print("")
 
     print("Power Status")
-    battery_api = SessyApi(SESSY_BATTERY_HOST, SESSY_BATTERY_USERNAME, SESSY_BATTERY_PASSWORD)
-    battery = SessyBattery(battery_api)
+    battery = SessyBattery(SESSY_BATTERY_HOST, SESSY_BATTERY_USERNAME, SESSY_BATTERY_PASSWORD)
     result = await battery.get_power_status()
     print(result)
     print("")
@@ -25,6 +23,6 @@ async def run():
     print(result)
     print("")
     
-    await battery_api.close()
+    await battery.close()
 
 asyncio.run(run())
--- sessypy/devices.py
+++ sessypy/devices.py
@@ -1,20 +1,21 @@
 from .const import SessyApiCommand
 from .api import SessyApi
 
-class SessyBattery():
-    def __init__(self, api: SessyApi):
-        self.api = api
+class SessyDevice():
+    def __init__(self, host, username: str, password: str):
+        self.api = SessyApi(host, username, password)
 
+    async def close(self):
+        await self.api.close()
+    
+class SessyBattery(SessyDevice):
     async def get_power_status(self):
         return await self.api.get(SessyApiCommand.POWER_STATUS)
     
     async def get_power_strategy(self):
         return await self.api.get(SessyApiCommand.POWER_STRATEGY)
 
-class SessyP1Meter():
-    def __init__(self, api: SessyApi):
-        self.api = api
-
+class SessyP1Meter(SessyDevice):
     async def get_p1_status(self):
         return await self.api.get(SessyApiCommand.P1_STATUS)
     
