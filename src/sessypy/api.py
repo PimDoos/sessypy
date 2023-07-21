4c6f05e04be494e27afe599fc79e3ebdff857175
Martijn <martijn@charged.nu>
2023-07-21 16:45:26 +0200

    Added more logging

--- src/sessypy/api.py
+++ src/sessypy/api.py
@@ -1,27 +1,30 @@
+import logging
 from aiohttp import BasicAuth, ClientConnectionError, ClientResponseError, ContentTypeError
 import aiohttp
 from .const import SessyApiCommand
 from .util import SessyConnectionException, SessyLoginException, SessyNotSupportedException
 
+_LOGGER = logging.getLogger(__name__)
 
 class SessyApi:
     def __init__(self, host, username, password):
         self.host = host
         self.username = username
-        
+
         self.session = aiohttp.ClientSession(
             auth=BasicAuth(username, password),
             raise_for_status = True
         )
-    
+
     async def get(self, command):
         return await self.request("GET", command)
     async def post(self, command: SessyApiCommand, data: dict = None):
         return await self.request("POST", command, data)
-    
+
     async def request(self, method: str, command: SessyApiCommand, data = None):
         try:
             url = self._command_url(command)
+            _LOGGER.debug(f"Requesting sessy with url: {url}, and data: {data}")
             response = await self.session.request(method, url, json = data)
             return await response.json()
         except ClientConnectionError:
@@ -33,11 +36,9 @@ class SessyApi:
                 raise SessyLoginException
             else:
                 raise SessyNotSupportedException
-        
-        
+
     def _command_url(self, command: SessyApiCommand):
         return f"http://{self.host}/{command.value}"
-    
+
     async def close(self):
         await self.session.close()
-    
